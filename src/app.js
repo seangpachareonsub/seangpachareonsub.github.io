f10f7184ed2e13f93ad18bc530d6e89f4e99c359
Kenn <kennkenns@hotmail.com>
2020-05-06 20:50:09 +0100

    landing

--- src/app.js
+++ src/app.js
@@ -1,35 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
 import 'bulma'
 import './styles/style.scss'
-import Baffle from 'baffle-react'
+
+import Landing from './components/Landing'
+
 
 
 
 const App = () => {
-  return (
-    // LANDING
-    <main>
-      <section>
 
-        <div>
-          <h1> SEANGPACHAREONSUB </h1>
-          <p> I'm Kenn. Let me lead you through who I am and what I do. </p>
-        </div>
+  return ( 
+    <> 
+    <Landing />
+    
+    <div className="about">
+      <header>
+        <span> No. 1</span>
+        <h1> WHO'S TALKING ?</h1>
+      </header>
+    </div>
 
-        <ion-icon name="arrow-down-sharp"></ion-icon>
-        <svg height="410" width="410">
-          <circle cx="205" cy="205" r="200" />  
-        </svg>
-      </section>
-
-
-      <section>
-        <h1> KS </h1>
-        <h1> EA </h1>
-        <h1> NG </h1>
-      </section>
-    </main>
+    </>
+    
   )
 }
 
---
+++ src/components/Landing.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react'
+import Rellax from 'rellax'
+
+
+const Landing = () => {
+
+  return (
+
+    <main>
+      <section>
+        <div>
+          <h1> SEANGPACHAREONSUB </h1>
+          <p> Hello world. Let me lead you through who I am and what I do. </p>
+        </div>
+
+        <ion-icon name="arrow-down-sharp"></ion-icon>
+        <svg height="410" width="410">
+          <circle cx="205" cy="205" r="200" />
+        </svg>
+      </section>
+
+  
+      <section>
+        <div> <h1> KS </h1> </div>
+        <div> <h1> EA </h1> </div>
+        <div> <h1> NG </h1> </div>
+      </section>
+    </main>
+  )
+}
+
+export default Landing
