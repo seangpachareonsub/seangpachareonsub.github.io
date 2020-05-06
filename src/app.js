b30044e31804f9d224d835141daadd7e2b098a9f
Kenn <kennkenns@hotmail.com>
2020-05-06 14:54:07 +0100

    landing

--- src/app.js
+++ src/app.js
@@ -8,7 +8,28 @@ import Baffle from 'baffle-react'
 
 const App = () => {
   return (
-    <h1> hello </h1>
+    // LANDING
+    <main>
+      <section>
+
+        <div>
+          <h1> SEANGPACHAREONSUB </h1>
+          <p> I'm Kenn. Let me lead you through who I am and what I do. </p>
+        </div>
+
+        <ion-icon name="arrow-down-sharp"></ion-icon>
+        <svg height="410" width="410">
+          <circle cx="205" cy="205" r="200" />  
+        </svg>
+      </section>
+
+
+      <section>
+        <h1> KS </h1>
+        <h1> EA </h1>
+        <h1> NG </h1>
+      </section>
+    </main>
   )
 }
 
