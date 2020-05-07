093662134c22be93bb869530ad8a9d029eba6a5f
Kenn <kennkenns@hotmail.com>
2020-05-07 14:00:04 +0100

    landing animation

--- src/app.js
+++ src/app.js
@@ -4,53 +4,38 @@ import 'bulma'
 import './styles/style.scss'
 
 import Landing from './components/Landing'
+import About from './components/About'
+
+
 import Rellax from 'rellax'
+import { TimelineLite, Expo } from 'gsap'
+import baffle from 'baffle'
+
+
+let a = baffle('.baffle', {
+  characters: '█▒░/>>£$@%*',
+  speed: 100
+}).start()
+a.reveal([1000], [1000])
+
+
 
 
 
 const App = () => {
+ 
+
+
+
+  
 
 
   return (
     <>
-      <Landing />
-
-      <div className="about">
-        <header>
-          <span> No. 1</span>
-          <h1> WHO'S TALKING ?</h1>
-        </header>
-
-        <section>
-          <div>
-            <p>
-              I'm Kenn, a Junior Full-stack Developer.
-              I create projects under the notion that less is more, and my work
-              reflects my enthusiasm for minimal and clean design.
-              Beyond cultivating my ability to write good code
-              and my perception in UI design, my aim is to take coding
-              into the professional dimension, where I hope to bring value and grow myself.
-            </p>
-          </div>
-
-          <div>
-            <h1> My work implements: </h1>
-            <ul>
-              <li> HTML, CSS(SASS), JSX </li>
-              <li> JavaScript </li>
-              <li> React </li>
-              <li> MERN & Django(DRF) Stack </li>
-              <li> PostgreSQL & MongoDB </li>
-              <li> Git & GitHub </li>
-            </ul>
-          </div>
-
-        </section>
-
-      </div>
+    
+     <Landing />
 
     </>
-
   )
 }
 
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,12 +1,42 @@
-import React, { useEffect } from 'react'
-import Rellax from 'rellax'
+import React, { useEffect, useState } from 'react'
+import { TimelineLite, Expo } from 'gsap'
 
 
 const Landing = () => {
 
-  // useEffect(() => {
-  //   const rellax = new Rellax('.rellax')
-  // }, [])
+  const [display, setDisplay] = useState(false)
+  const [counter, setCounter] = useState(0)
+
+  useEffect(() => {
+
+    const count = setInterval(() => {
+      setCounter(counter => {
+        counter < 100 ? setCounter(counter + 1) : (
+          clearInterval(count),
+          setCounter(100),
+          reveal()
+        )
+      })
+    }, 30)
+  }, [])
+
+
+  function reveal() {
+
+    const t1 = new TimelineLite({
+      onComplete: () => setTimeout(() => {
+        setDisplay(true)
+      }, 1000)
+
+    })
+
+    t1
+      .to('.content', 1, { width: '100%', ease: Expo.easeInOut }, '+=0.7')
+      .to('.hide', 0.1, { display: 'none' })
+      .to('.content', 0.7, { height: '100vh', ease: Expo.easeInOut }, '+=0.5')
+      .to('.sections', 0.5, { opacity: 1 })
+
+  }
 
   function scroll() {
     window.scrollTo(0, 733)
@@ -15,26 +45,41 @@ const Landing = () => {
   return (
 
     <main>
-      <section>
-        <div>
-          <h1> SEANGPACHAREONSUB </h1>
-          <p> Hello world. Let me take you through who I am and what I do. </p>
-        </div>
 
-        <span onClick={scroll}> Click to scroll </span>
-        <ion-icon onClick={scroll} name="arrow-down-sharp"></ion-icon>
-        <svg onClick={scroll} height="410" width="410">
-          <circle cx="205" cy="205" r="200" />
-        </svg>
-      </section>
+      <div className='loading'>
+        <div className='hide' id='initial' style={{ width: counter + '%' }} ></div>
+        <h1 className='hide'> {counter} </h1>
+      </div>
 
-  
-      <section>
-        <div> <p> KS </p> </div>
-        <div> <p> EA </p> </div>
-        <div> <p> NG </p> </div>
-      </section>
+
+      <div className='content'>
+
+        <section className='sections'>
+          <div>
+            <h1> SEANGPACHAREONSUB </h1>
+            <p> Hello world. Let me take you through who I am and what I do. </p>
+          </div>
+
+          <span onClick={scroll}> Click to scroll </span>
+          <ion-icon onClick={scroll} name="arrow-down-sharp"></ion-icon>
+          <svg onClick={scroll} height="410" width="410">
+            <circle cx="205" cy="205" r="200" />
+          </svg>
+        </section>
+
+        <section className='sections'>
+          <div> <p> KS </p> </div>
+          <div> <p> EA </p> </div>
+          <div> <p> NG </p> </div>
+        </section>
+
+      </div>
+
+
+
     </main>
+
+
   )
 }
 
